43adc8b5b66de139030d4482870237a2ba1a0c77
JamesKimari <jamesgkimari@gmail.com>
2018-02-24 20:01:36 +0300

    created the Location and Category models and related them to the Image model

--- gallery/models.py
+++ gallery/models.py
@@ -1,20 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=20)        
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length=20)    
+
+    def __str__(self):
+        return self.name
+
 class Image(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=50)
+    category = models.ForeignKey('Category', on_delete = models.CASCADE, null='True', blank=True)
+    location = models.ForeignKey('Location', on_delete = models.CASCADE, null='True', blank=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
         ordering = ['name']
-
-class Category(models.Model):
-    name = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name
 
 
