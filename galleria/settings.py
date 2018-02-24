4d771d7226226dfb675c1b6c9edb8970ad3ddc0a
JamesKimari <jamesgkimari@gmail.com>
2018-02-24 18:59:22 +0300

    added postgreql configurations to project settings and connected postgresql to the application

--- galleria/settings.py
+++ galleria/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'galleria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'galleria',
+        'USER': 'Pasaris',
+        'PASSWORD': 'maisiewilliams7'
     }
 }
 
@@ -110,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
