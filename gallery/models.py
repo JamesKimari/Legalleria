37c2fe24b94e1f865bc98d2073e823da0099245a
JamesKimari <jamesgkimari@gmail.com>
2018-02-24 19:20:55 +0300

    created the category model and made migrations to the database

--- gallery/models.py
+++ gallery/models.py
@@ -5,7 +5,16 @@ class Image(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         ordering = ['name']
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
 
 
