9a3acb9d3ec2d6e8b2547a48a8287bdd08e47ef6
JamesKimari <jamesgkimari@gmail.com>
2018-02-25 15:27:17 +0300

    wrote a view on the home page

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Image
 
 # Create your views here.
 def gallery(request):
-    return render(request, 'gallery.html')
+    all_pics = Image.display_all()
+    return render(request, 'gallery.html', {"all_pics":all_pics})
