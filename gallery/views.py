b6ff80f8d27d45d27972bae291046708a146d63e
JamesKimari <jamesgkimari@gmail.com>
2018-02-27 20:03:23 +0300

    defined view functions: search results, image categories and locations

--- gallery/views.py
+++ gallery/views.py
@@ -3,5 +3,27 @@ from .models import Image
 
 # Create your views here.
 def gallery(request):
-    all_pics = Image.display_all_pics()
+    all_pics = Image.all_pics()
     return render(request, 'gallery.html', {"all_pics":all_pics})
+
+def search_results(request):
+    if 'image' in request.GET and request.GET['image']:
+        search_input = request.GET.get('image')
+        searched_images = Image.search_by_category(search_input)
+        message = f"{search_input}"
+
+        return render(request, 'search.html', {"message":message, "images":searched_images})
+
+    else:
+        message = "Please input something in the search field"
+        return render(request, 'search.html', {'message':message})
+
+def display_images_categories(request):    
+    pics = Image.pic_categories()
+
+    return render(request, 'categories.html', {"pics":pics}) 
+
+def display_images_locations(request):    
+    pics = Image.pic_locations()
+
+    return render(request, 'locations.html', {"pics":pics}) 
