3ded2201ca7baac56da70642fd8f4f653a2fa53a
JamesKimari <jamesgkimari@gmail.com>
2018-02-28 09:13:48 +0300

    added a urlconf for directing us to the single_pic.html

--- gallery/urls.py
+++ gallery/urls.py
@@ -7,7 +7,8 @@ urlpatterns=[
     path('', views.gallery, name = 'gallery'),
     path('search/', views.search_results, name = 'search_results'),
     path('categories/', views.display_images_categories, name = 'categories'),
-    path('locations/', views.display_images_locations, name = 'locations')
+    path('locations/', views.display_images_locations, name = 'locations'),
+    path('image/<int:image_id>', views.single_pic, name = 'single_pic')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
